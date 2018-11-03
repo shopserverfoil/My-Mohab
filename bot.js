79a2137d60be7d41e5506f6861beb6207f31988d
hamohoba573 <43551292+hamohoba573@users.noreply.github.com>
2018-11-03 16:24:32 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,68 +1,18 @@
 const Discord = require('discord.js');
 
+const fs = require('fs')
+
 const client = new Discord.Client();
 
- 
+const prefix = '';
 
 client.on('ready', () => {
 
-client.user.setActivity("سبام كل يوم 350 آلف كريديت",{type: 'PLAYING'});
+ client.user.setActivity("سبام كل يوم 350 آلف كريديت",{type: 'PLAYING'});
+    
+ console.log(`Logged in as [ Pharaohs]`);
 
-     
 });
-  console.log(`Logged in as ${client.user.tag}!`);
-
-
- 
-
-
- 
-
-
-
-
-
-
-
-
- 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
@@ -71,4 +21,3 @@ client.user.setActivity("سبام كل يوم 350 آلف كريديت",{type: 'P
 
 
 client.login(process.env.BOT_TOKEN);
-
