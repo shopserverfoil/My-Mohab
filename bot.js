f309013788cecabff7ddc407601b99ec520706ee
alialmoftery <43893786+alialmoftery@users.noreply.github.com>
2018-11-19 02:29:37 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,11 +4,11 @@ const fs = require('fs')
 
 const client = new Discord.Client();
 
-const prefix = '';
+const prefix = '#';
 
 client.on('ready', () => {
 
- client.user.setGame(`Vida 1000K Soon `,'https://www.twitch.tv/MeeRcY')
+ client.user.setGame(`مشغول جدا اعمل طابور عشان ارد مرة واحده `,'https://www.twitch.tv/MeeRcY')
  
  console.log(`Logged in as [ Pharaohs]`);
 
