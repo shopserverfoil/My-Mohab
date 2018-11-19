fabae0f40782df1101ca23fe23e92f2fb1bd8036
alialmoftery <43893786+alialmoftery@users.noreply.github.com>
2018-11-19 02:45:27 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ const prefix = '#';
 
 client.on('ready', () => {
 
- client.user.setGame(`مشغول        `,'https://www.twitch.tv/MeeRcY')
+ client.user.setGame(`نايم`,'https://www.twitch.tv/MeeRcY')
  
  console.log(`Logged in as [ Pharaohs]`);
 
