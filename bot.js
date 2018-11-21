52bde888b534d2bfb9941ffee3006d97e44f459a
shopserverfoil <44735465+shopserverfoil@users.noreply.github.com>
2018-11-21 02:54:09 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,47 @@
+const Discord = require('discord.js');
+
+const fs = require('fs')
+
+const client = new Discord.Client();
+
+const prefix = '';
+
+client.on('ready', () => {
+
+
+ 
+ console.log(`Logged in as [ Pharaohs]`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+const adminprefix = "#";
+const devs = ['472040614519898133'];
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!devs.includes(message.author.id)) return;
+if (message.content.startsWith(adminprefix + 'st')) {
+  client.user.setGame(argresult, "https://www.twitch.tv/idk")
+    message.channel.sendMessage(`**:white_check_mark: : ${argresult}**`)
+}
+});
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
