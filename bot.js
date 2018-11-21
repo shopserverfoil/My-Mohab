1a0517ab7e0fde213dd589188f92a48de8b91add
shopserverfoil <44735465+shopserverfoil@users.noreply.github.com>
2018-11-21 03:02:10 +0200

    Create bot.js

---
+++ bot.js
@@ -0,0 +1,48 @@
+const Discord = require('discord.js');
+
+const fs = require('fs')
+
+const client = new Discord.Client();
+
+const prefix = '#';
+
+client.on('ready', () => {
+
+ client.user.setGame(`مشغول`,'https://www.twitch.tv/MeeRcY')
+ 
+ console.log(`Logged in as [ Pharaohs]`);
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.BOT_TOKEN);
